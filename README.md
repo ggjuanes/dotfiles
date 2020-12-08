e52e28749a5327cf5be4d200562d8488e28348de
Guillermo Garcia Juanes <ggjuanes@gmail.com>
2020-12-08 11:00:31 +0100

    Initial commit: README.md, .gitignore, and installer

---
+++ README.md
@@ -0,0 +1,8 @@
+# 🚀 ggjuanes' dotfiles
+
+[![License](https://img.shields.io/github/license/ggjuanes/dotfiles)](LICENSE)
+
+# Inspired by
+- https://github.com/CodelyTV/dotfiles
+- https://github.com/mathiasbynens/dotfiles
+- https://github.com/alrra/dotfiles
---
+++ installer.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+function doIt() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude ".gitignore" \
+		--exclude "installer.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE" \
+		-avh --no-perms . ~;
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then
+	doIt;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		doIt;
+	fi;
+fi;
+unset doIt;
