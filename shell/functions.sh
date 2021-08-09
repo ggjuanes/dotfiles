cda336f84ffa5c937f53791018c1206c027abbb6
Guillermo Garcia Juanes <ggjuanes@gmail.com>
2021-08-09 11:10:16 +0200

    Reverse search with fzf

--- shell/functions.sh
+++ shell/functions.sh
@@ -16,3 +16,13 @@ sp() {
 
   cd "$project" || exit
 }
+
+_reverse_search() {
+  local selected_command
+  selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | fzf)
+
+  LBUFFER=$selected_command
+}
+
+zle -N _reverse_search
+bindkey '^r' _reverse_search
