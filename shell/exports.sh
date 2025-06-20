72e846a2095cb5269c3ba7ceb100452a357ace0b
Guillermo Garcia Juanes <ggjuanes@gmail.com>
2025-06-20 18:05:33 +0200

    chore(common): clean up shell stuff not used

--- shell/exports.sh
+++ shell/exports.sh
@@ -1,3 +1,3 @@
 export GPG_TTY=$(tty)
-export PATH=~/shell/scripts/aws:/usr/local/Cellar/openvpn/2.5.3/sbin:/opt/homebrew/bin:$PATH
+export PATH=~/shell/scripts/aws:/opt/homebrew/bin:$PATH
 export DOTFILE_CONFIG_FILE=~/dotfiles_config.json
--- shell/functions.sh
+++ shell/functions.sh
@@ -37,25 +37,6 @@ function _export_config_value() {
   DOTFILE_CONFIG_VALUE=$(jq -r "$1" "$DOTFILE_CONFIG_FILE")
 }
 
-function _connect_vpn() {
-  local env=""
-  _export_config_value '.vpn.directory'
-  local dir=$DOTFILE_CONFIG_VALUE
-  env=$(ls $dir | fzf)
-
-  local tmpfile=""
-  tmpfile="/tmp/vpn_connect.XXXXXXX"
-  tmpfile=$(mktemp "$tmpfile")
-  _export_config_value '.vpn.user'
-  echo "$DOTFILE_CONFIG_VALUE" >> "$tmpfile"
-
-  printf 'Enter vpn code : '
-  read -r password
-  echo "$password" >> $tmpfile
-
-  sudo openvpn --config "${dir}/${env}" --auth-user-pass "$tmpfile"
-}
-
 function _reverse_search() {
   local selected_command
   selected_command=$(fc -rl 1 | awk '{$1="";print substr($0,2)}' | fzf)
